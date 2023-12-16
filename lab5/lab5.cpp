0d59909acafc5f48f425b4e438ef8f1768e3d243
ESSemykina <90087300+ESSemykina@users.noreply.github.com>
2023-12-16 18:54:32 +0200

    Add files via upload

---
+++ lab5/lab5.cpp
@@ -0,0 +1,34 @@
+﻿#include "FileUserRepository.h"
+#include "UserManager.h"
+
+int main() 
+{
+    setlocale(LC_ALL, "Russian");  
+
+    std::string filePath = "users.txt";
+
+    std::unique_ptr<IUserRepository> userRepository(new FileUserRepository(filePath));
+
+    std::unique_ptr<UserManager> userManager(new UserManager(userRepository.get()));
+
+    User user(1, "KatyaSemykina", "password12345", "Test");
+
+    userManager->signIn(user);
+
+    std::cout << user.getLogin() << '\n';
+
+    bool isAuthorized = userManager->isAuthorized();
+    if (isAuthorized)
+    {
+        
+        std::cout << "User is authorized" << std::endl;
+    }
+    else {
+     
+        std::cout << "User is not authorized" << std::endl;
+    }
+
+    userManager->signOut(user);
+
+    return 0;
+}
